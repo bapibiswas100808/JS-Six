2569d1190586ea4aa0c5b7211f4fc1a213203e6c
Bapi Biswas <star.bappy@gmail.com>
2023-01-09 18:05:36 +0600

    homework done

---
+++ script.js
@@ -0,0 +1,129 @@
+// Function Declearation
+// function calculateAge(birthYear) {
+//   return 2022 - birthYear;
+// }
+// const age1 = calculateAge(1988);
+// console.log(age1);
+
+// function schoolJoin(birthyear) {
+//   const ageNeed = 2022 - birthyear;
+//   return ageNeed;
+// }
+// const student1 = schoolJoin(2014);
+// console.log(student1);
+
+// function totalFruits(numberoffruits) {
+//   const fruitsold = 100 - numberoffruits;
+//   return fruitsold;
+// }
+// const basket1 = totalFruits(90);
+// console.log(basket1);
+
+// function presentStudent(numberofstudent) {
+//   const absentstudents = 50 - numberofstudent;
+//   return absentstudents;
+// }
+// const class1 = presentStudent(43);
+// console.log(class1);
+
+// function expression
+const calculateAgenew = function (birthYear) {
+  return 2022 - birthYear;
+};
+const age2 = calculateAgenew(1975);
+console.log(age2);
+
+const fruitsold = function (numberoffruits) {
+  return 100 - numberoffruits;
+};
+
+const basket1 = fruitsold(40);
+console.log(basket1);
+
+// Function Declearation
+// const age1 = calculateAge(1988);
+// function calculateAge(birthYear) {
+//   return 2022 - birthYear;
+// }
+// console.log(age1);
+// // function expression
+
+// const calculateAgenew = function (birthYear) {
+//   return 2022 - birthYear;
+// };
+// const age2 = calculateAgenew(1975);
+// console.log(age2);
+
+// Arrow Function
+// Example 1
+// const calculateAgeAnother = (birthyear) => 2022 - birthyear;
+// const age1 = calculateAgeAnother(1960);
+// console.log(age1);
+
+// // example 2
+// const calculateAgeAnother1 = (birthyear) => {
+//   return 2022 - birthyear;
+// };
+// const age2 = calculateAgeAnother1(1980);
+// console.log(age2);
+
+// // Examle 3
+// const jobleft = (birthyear) => {
+//   const age = 2022 - birthyear;
+//   return 62 - age;
+// };
+// const ageLeft = jobleft(1972);
+// console.log(ageLeft);
+// console.log(`A job age left ${ageLeft} years.`);
+
+// Array
+// const student1 = "Asif";
+// const student2 = "Rakib";
+// const student3 = "Rahim";
+
+// console.log(student1);
+
+// // array example
+// const students = ["Asif", "Rakib", "Rahim", "A", "B", "C", "D"];
+// console.log(students);
+// console.log(students[0]);
+// console.log(students[2]);
+
+// students[0] = "Karim";
+// console.log(students);
+// console.log(students.length);
+// console.log(students[students.length - 1]);
+
+// // array example 2
+// const players = new Array("Sakib", "Tamim", "Miraz");
+// console.log(players[1]);
+
+// const user = ["Bapi", "Biswas", 1991, 2022 - 1991, "Learning web development"];
+// console.log(user);
+// console.log(user[3]);
+
+// Array method
+// const users = ["user1", "user2", "user3"];
+// console.log(users);
+// // push method
+// users.push("user4");
+// console.log(users);
+
+// // unshift method
+// users.unshift("firstuser");
+// console.log(users);
+
+// // pop method
+// users.pop();
+// console.log(users);
+// // shift method
+// users.shift();
+// console.log(users);
+
+// const removeItem = users.pop();
+// console.log(users);
+// console.log(removeItem);
+
+// const removeItem1 = users.shift();
+// console.log(users);
+// console.log(removeItem1);
